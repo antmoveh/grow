f42797126241d965c02cee85b6ea0a14e27416b3
付少松 <fushaosong@gridsum.com>
2019-08-23 16:12:27 +0800

    svc_name

--- application/views/apollo.py
+++ application/views/apollo.py
@@ -1,12 +1,12 @@
 
 from . import *
-from config import client, namespace, node_ip, pod_namespace, pod_ip, pod_name
+from config import client, namespace, node_ip, pod_namespace, pod_ip, pod_name, svc_name
 
 
 # 获取apollo 参数
 @response_json
 def apollo_values():
-    resp = {"node_ip": node_ip, "pod_name": pod_name, "pod_namespace": pod_namespace, "pod_ip": pod_ip}
+    resp = {"node_ip": node_ip, "pod_name": pod_name, "pod_namespace": pod_namespace, "pod_ip": pod_ip, "svc_name": svc_name}
     request_json = request.get_json(force=True)
     for key in request_json:
         resp[key] = client.get_value(key, "default", namespace=namespace)
